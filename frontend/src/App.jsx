30541d20f6906284d2998e0e18614997fb524555
Thibaut <thibaut.mosteau@lilo.org>
2023-10-26 14:12:14 +0200

    mise à jour css media pour radio player

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
-import "./App.css";
+import RadioPlayer from "./components/RadioPlayer";
 
 function App() {
-  return (
-    <div className="App">
+  return <RadioPlayer />;
+}
+
+export default App;
+
+/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React !</p>
@@ -35,8 +36,4 @@ function App() {
           </a>
         </p>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */
