4cfce95cb5488653d36a55a7b6e7e167bdbc1dfa
hlnvrnt <helene.vernet@live.fr>
2023-11-08 09:05:27 +0100

    commit change

--- frontend/src/components/DisplayRadio.jsx
+++ frontend/src/components/DisplayRadio.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import SearchBar from "./SearchBar";
+import { createPortal } from "react-dom"
 
 function DisplayRadio({ radiosRandom }) {
   const [searchValue, setSearchValue] = useState("");
